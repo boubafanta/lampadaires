f19339e167b8fc39651d1c4ec635b7a7fbce8c27
maguy <maguette.ndong@gmail.com>
2014-11-28 12:29:03 +0100

    Function iti OK v2

---
+++ sql/requete_28112014.sql
@@ -0,0 +1,89 @@
+﻿SELECT * FROM quartier
+
+--Liste des lampadaires d'un état X d'un quartier Y
+create or replace function etat_quartier(etat text, quartier integer)
+  returns table (param_id integer, param_states text, param_geom geometry)
+as
+$body$
+	SELECT l.gid,l.states,l.geom
+	FROM quartier AS q
+	JOIN lampadaire AS l
+	ON st_dwithin(l.geom,q.geom,0)
+	WHERE l.states = $1 
+	AND q.gid = $2
+$body$
+language sql;
+
+-- Test : SELECT * FROM etat_quartier('ok',2)
+
+--============== Afficher l'itinéraire d'un point A à un point B en passant par le chemin le plus court éclairé à X% ===============
+
+-- Step 1 = on crée la vue des troncons eclaires
+CREATE OR REPLACE VIEW public.tronconec AS 
+ SELECT t.gid,
+    t.nam,
+    t.id_start,
+    t.id_end,
+    t.typ,
+    t.v_moy,
+    COALESCE(te.coeff_eclairage, 0::double precision) AS coeff_eclairage,
+    t.geom
+   FROM troncon t
+     LEFT JOIN troncon_eclairage te ON t.gid = te.id_troncon;
+
+-- Step 2 = on crée la fonction associée avec les paramètres en entrée A, B et X
+
+create or replace function itineraireb(A integer, B integer, C float)
+  returns table (param_seq integer, param_node integer, param_edge integer, param_cost float, param_coef float, param_geom text)
+as
+$body$
+	SELECT seq, id1 as node, id2 as edge,  cost , coeff_eclairage, st_asgeojson(geom) FROM pgr_dijkstra('
+		SELECT gid AS id,
+			id_start::integer as source,
+			id_end::integer as target,
+			st_length(geom) as cost,
+			coeff_eclairage
+			FROM tronconec where coeff_eclairage>='||$3,
+		$1,$2,false,false)
+join tronconec as tr
+on id2 = tr.gid
+$body$
+language sql;
+
+-- Step 3 = Récupérer l'id du point le plus proche des coordonnées (X,Y)
+create or replace function pointD(X float, Y float)
+  returns integer
+as	
+$body$
+	SELECT gid 
+	FROM voirie_plane_noeud AS t
+	ORDER BY t.geom <-> ST_transform(ST_setsrid(ST_MakePoint($2,$1),4326),32628)
+	LIMIT 1
+$body$ 
+language sql;
+
+-- Step 4 = Récupérer le tronçon
+create or replace function itib(X1 float, Y1 float,X2 float, Y2 float, C float)
+  returns table (param_seq integer, param_node integer, param_edge integer, param_cost float, param_coef float, param_geom text)
+as
+$body$
+	--with n1 as (SELECT * FROM point($1,$2)), n2 as (SELECT * FROM point($3,$4))
+	SELECT * FROM itineraireb(pointD($1,$2),pointD($3,$4),$5)
+$body$
+language sql;
+
+
+-- Test 
+SELECT *
+FROM itib(14.66439117,-17.44131088,14.70358007,-17.45264053,0)
+
+SELECT *
+FROM pointD(14.66439117,-17.44131088)
+
+SELECT *
+FROM pointD (14.70358007,-17.45264053)
+
+SELECT * FROM itineraire(1910,970,0)
+
+SELECT * FROM voirie_plane_noeud
+WHERE gid=1910
